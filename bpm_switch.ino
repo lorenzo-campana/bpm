8c93244cf98587c58a54b1adeb1e05db120c40f1
lorenzo-campana <54334028+lorenzo-campana@users.noreply.github.com>
2019-10-10 12:17:31 -0700

    Update bpm_switch.ino

--- bpm_switch.ino
+++ bpm_switch.ino
@@ -1,7 +1,10 @@
-// This Arduino sketch is used to control a minicircuit USB / TTL RF SP16T Switch, a 16 channel multiplexer. The parameter are set through serial comunication between the arduino and a Raspberry Pi with installed EPICS, the ASYNC and StreamRecord libraries. 
+// This Arduino sketch is used to control a minicircuit USB / TTL RF SP16T Switch, a 16 channel multiplexer. The parameter are set 
+// through serial comunication between the arduino and a Raspberry Pi with EPICS, the ASYNC and StreamRecord libraries installed. 
 //The program features two different operation modes: 
-//-- MANUAL: this mode lets you decide which channel of the 16 of the multiplexer you want as output. 
-// -- AUTO: this mode lets ypu select one or more channel of the multiplexer. The arduino will continuously switch between them, after it detects a trigger signal. you can set how long a channel is selected as output (dwell time), how many times you want to switch between the selected channels (cycles), and the delay from the trigger signal.
+// -- MANUAL: this mode lets you decide which channel of the 16 of the multiplexer you want as output. 
+// -- AUTO: this mode lets you select one or more channels of the multiplexer. The arduino will continuously switch between them, 
+//          after it detects a trigger signal. you can set how long a channel is selected as output (dwell time), how many times 
+//          you want to switch between the selected channels (cycles), and the delay from the trigger signal (global_delay).
 
 #include <avr/interrupt.h>  
 #include <stdio.h>
@@ -17,23 +20,24 @@ const int INT_PIN = 2 ; // define the pin for the interrupt
 
 String input = ""; // variable for the serial input commands
 
-// declaration of the global variable that will be used as parameter
+// declaration of the global variables that will be used as parameters
 
-int mode_select=1; // manual mode -> 1 Automatic mode -> 0
-long int dwell_time=100; // time that the arduino waits before switching to anoter channel in automatic mode
-long int cycles=10; // number of time the arduino will go through the selected channels while switching
+int mode_select=1; // manual mode -> 1 automatic mode -> 0
+long int dwell_time=100; // time to wait before switching to another channel in automatic mode
+long int cycles=10; // number of time to go through the selected channels while switching
 int channel_select=1; // channel selection for manual mode. Accept a number between 1 and 16
-long int channel_mask=65535; // channel selection for automatic mode. Accept a number between 1 and 65535. To select the channels you want, convert a 16 bit binary number to decimal. eg 65535 = 1111111111111111 all channel selected
-char error[40]; // string used for comunicating back from the uno to the pi
-int global_delay=10; // time that the arduino waits after receiving the 10 trigger before starting switching
+long int channel_mask=65535; // channel selection for automatic mode. Accept a number between 1 and 65535. To select the channels 
+                             // you want, convert a 16 bit binary number to decimal. eg 65535 = 1111111111111111 all channel selected
+char error[40]; // string used for communicating from the uno to the pi
+int global_delay=10; // time to wait after receiving the 10 Hz trigger before starting switching
 
-int mask=0b1; // binary number used to scan the cahnnel_mask in auto mode
+int mask=0b1; // binary number 1 used to scan the cahnnel_mask in auto mode
 int i; // for loop indexes
 int j;
 
-int volatile trigger=0; // variable that activate automatic mode when it is selected and a 10 hz trigger comes
+int volatile trigger=0; // variable that activates automatic mode when it is selected and a 10 hz trigger comes
 
-// C union struct used to convert the decimal channel selction number in binary.
+// C union struct used to convert the decimal channel selection number in binary.
 union data {
     struct{
         unsigned int a:1;
@@ -50,33 +54,34 @@ union data converter2;
 
 
 void setup() {
-    pinMode(D0, OUTPUT); //setup for te outup pins
+    pinMode(D0, OUTPUT); // setup for te output pins
     pinMode(D1, OUTPUT);
     pinMode(D2, OUTPUT);
     pinMode(D3, OUTPUT);
     pinMode(D4, OUTPUT);
 
-    pinMode(INT_PIN, INPUT_PULLUP); //setup for the trigger input pin
+    pinMode(INT_PIN, INPUT_PULLUP); // setup for the trigger input pin
     
-    attachInterrupt(digitalPinToInterrupt(INT_PIN), interrupt10hz, FALLING); //define the trigger pin, the trigger edge and the function to be executed when the trigger comes
-    Serial.begin(9600); // Make sure the Serial commands match this baud rate
+    attachInterrupt(digitalPinToInterrupt(INT_PIN), interrupt10hz, FALLING); // define the trigger pin, the trigger edge and the function
+                                                                             // to be executed when the trigger comes
+    Serial.begin(9600); // make sure the serial commands match this baud rate
 }
 
 void loop () {
-    while (Serial.available()>0){ //detect if something comes through the serial
-        char lastRecvd = Serial.read(); //save the command in a variable
+    while (Serial.available()>0){ // detect if something comes through the serial
+        char lastRecvd = Serial.read(); // save the command in a variable
         
-        if (lastRecvd == '\n') { // The end of the command has arrived
+        if (lastRecvd == '\n') { // the end of the command has arrived
             switch (input[0]) {
 
-                case 'D':// Dwell time has come in
+                case 'D':// dwell time has come in
                     input = input.substring(1,input.length()); 
                     dwell_time = input.toInt(); // convert the dwell time into a int
-                    sprintf(error, "dwell_changed_to_%d_us\n", dwell_time); // write into the error string what changed
+                    sprintf(error, "dwell_changed_to_%d_us\n", dwell_time); // write to the error string what changed
                     input = ""; // reset the input
                     break;
 
-                case 'T':// Mode select has come in
+                case 'T':// mode select has come in
                     input = input.substring(1,input.length());
                     mode_select = input.toInt();
                     if(mode_select==0) 
@@ -93,7 +98,7 @@ void loop () {
                     sprintf(error, "chennel_selected_%d\n", channel_select);
                     if (mode_select==1) { 
 
-                        converter1.value=channel_select; // convert the channel_select to a binary variable
+                        converter1.value=channel_select; // convert channel_select to a binary variable
                         digitalWrite(D0, converter1.outputs.a); // set the pins according to the channel selected
                         digitalWrite(D1, converter1.outputs.b);
                         digitalWrite(D2, converter1.outputs.c);
@@ -125,7 +130,7 @@ void loop () {
                     input = "";
                     break;
 
-                case 'R': // A read command has come in
+                case 'R': // a read command has come in
                     Serial.print("R "); 
                     Serial.print(error); //send the error string through the serial
                     Serial.print("\n");
@@ -137,7 +142,7 @@ void loop () {
             }// end switch
         }// end if
 
-        else { // Input is still coming in
+        else { // input is still coming in
             input += lastRecvd;
         }
     }
@@ -165,12 +170,12 @@ void loop () {
             digitalWrite(D1,LOW);
             digitalWrite(D2,LOW);
             digitalWrite(D3,LOW);
-            digitalWrite(D4,LOW);
-        }
+            digitalWrite(D4,LOW);   
+           }
     }
 }
 
 
-void interrupt10hz() {
+void interrupt10hz() { // 
     trigger=1; 
 }
